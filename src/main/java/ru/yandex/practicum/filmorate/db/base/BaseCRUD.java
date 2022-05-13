bec9e830bd710e5fc2748d2388bc8b3a187c2ab1
AlexeyShirokikh <shirokih-1997@mail.ru>
2022-05-13 21:44:56 +0300

    feat: интерфейсы crud-методов

--- src/main/java/ru/yandex/practicum/filmorate/db/base/BaseCRUD.java
+++ src/main/java/ru/yandex/practicum/filmorate/db/base/BaseCRUD.java
@@ -1,11 +1,11 @@
 package ru.yandex.practicum.filmorate.db.base;
 
 public interface BaseCRUD <model, primaryKeyType>{
-    public model read(primaryKeyType id);
+    model read(primaryKeyType id);
 
-    public void create(model object);
+    void create(model object);
 
-    public void update(primaryKeyType id, model updatedObject);
+    void update(primaryKeyType id, model updatedObject);
 
-    public void delete(primaryKeyType id);
+    void delete(primaryKeyType id);
 }
