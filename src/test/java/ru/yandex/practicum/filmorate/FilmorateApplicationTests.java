32c0553d9d860863d2da602e64e5bba8ad23028c
ShirokihAlexey <shirokihaleksey@yandex.ru>
2022-07-01 21:21:56 +0300

    fix: правки в тестах

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,12 +1,15 @@
 package ru.yandex.practicum.filmorate;
 
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.db.dao.FilmDbStorage;
+import ru.yandex.practicum.filmorate.db.dao.RatingDbStorage;
 import ru.yandex.practicum.filmorate.db.dao.UserDbStorage;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -28,31 +31,35 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class FilmoRateApplicationTests {
 	private final UserDbStorage userStorage;
 	private final FilmDbStorage filmStorage;
+	private final RatingDbStorage ratingStorage;
 
 	@Test
 	public void testCreateUser()  throws ValidationException {
-		Assertions.assertFalse(userStorage.contains(1));
 		User testUser = new User("test@test.test", "test", "test", LocalDate.of(2000, 1, 1));
-		userStorage.create(testUser);
-		Assertions.assertTrue(userStorage.contains(1));
+		User created = userStorage.create(testUser);
+		Assertions.assertTrue(userStorage.contains(created.getId()));
 	}
 
 	@Test
-	public void testFindUserById() throws NotFoundException {
-		User user = userStorage.read(1);
-		Assertions.assertEquals(user, new User(1, "test@test.test", "test", "test",
-				LocalDate.of(2000, 1, 1)));
+	public void testFindUserById() throws NotFoundException, ValidationException {
+		User testUser = new User("testFind@test.test", "testFind", "testFind",
+				LocalDate.of(2000, 1, 1));
+		User created = userStorage.create(testUser);
+
+		User user = userStorage.read(created.getId());
+		Assertions.assertEquals(user, created);
 	}
 
 	@Test
 	public void testUpdateUser() throws NotFoundException, ValidationException {
-		User user = userStorage.read(1);
-		user.setLogin("testNew");
+		User testUser = new User("test@test.test", "test", "test", LocalDate.of(2000, 1, 1));
+		User createdUser = userStorage.create(testUser);
+		createdUser.setLogin("testNew");
 
-		userStorage.update(user);
+		userStorage.update(createdUser);
 
-		User updatedUser = userStorage.read(1);
-		Assertions.assertEquals(user, updatedUser);
+		User updatedUser = userStorage.read(createdUser.getId());
+		Assertions.assertEquals(createdUser, updatedUser);
 	}
 
 	@Test
@@ -67,37 +74,49 @@ class FilmoRateApplicationTests {
 
 	@Test
 	public void testDeleteUser() throws NotFoundException, ValidationException {
-		userStorage.delete(1);
-		assertThrows(NotFoundException.class, () -> userStorage.read(1));
+		User testUser = new User("test@test.test", "test", "test", LocalDate.of(2000, 1, 1));
+		User created = userStorage.create(testUser);
+		Assertions.assertTrue(userStorage.contains(created.getId()));
+
+		userStorage.delete(created.getId());
+		Assertions.assertFalse(userStorage.contains(created.getId()));
 	}
 
 	@Test
 	public void testCreateFilm()  throws ValidationException {
-		Assertions.assertFalse(filmStorage.contains(1));
 		Film testFilm = new Film("Test", "Test", LocalDate.of(2000, 1, 1), 1, 1, new Rating(1, "test", "test"), null);
-		filmStorage.create(testFilm);
-		Assertions.assertTrue(filmStorage.contains(1));
+		Film createdFilm = filmStorage.create(testFilm);
+		Assertions.assertTrue(filmStorage.contains(createdFilm.getId()));
 	}
 
 	@Test
-	public void testFindFilmById() throws NotFoundException {
-		Film film = filmStorage.read(1);
-		Assertions.assertEquals(film, new Film("Test", "Test", LocalDate.of(2000, 1, 1), 1, 1, new Rating(1, "test", "test"), null));
+	public void testFindFilmById() throws NotFoundException, ValidationException {
+		Film testFilm = new Film("Test", "Test", LocalDate.of(2000, 1, 1), 1, 1, new Rating(1, "test", "test"), null);
+		Film created = filmStorage.create(testFilm);
+		Rating rating = ratingStorage.read(created.getMpa().getId());
+		created.setMpa(rating);
+
+		Film film = filmStorage.read(created.getId());
+		Assertions.assertEquals(film, created);
 	}
 
 	@Test
 	public void testUpdateFilm() throws NotFoundException, ValidationException {
-		Film film = filmStorage.read(1);
-		film.setName("testNew");
+		Film firstFilm =  new Film("Test", "Test", LocalDate.of(2000, 1, 1), 1, 1, new Rating(1, "test", "test"), null);
+		Film createdFilm = filmStorage.create(firstFilm);
+		createdFilm.setName("testNew");
+		Rating rating = ratingStorage.read(firstFilm.getMpa().getId());
+		createdFilm.setMpa(rating);
+		filmStorage.update(createdFilm);
 
-		filmStorage.update(film);
-
-		Film updatedFilm = filmStorage.read(1);
-		Assertions.assertEquals(film, updatedFilm);
+		Film updatedFilm = filmStorage.read(createdFilm.getId());
+		Assertions.assertEquals(createdFilm, updatedFilm);
 	}
 
 	@Test
 	public void testGetAllFilms() throws NotFoundException, ValidationException {
+		Film firstFilm =  new Film("Test", "Test", LocalDate.of(2000, 1, 1), 1, 1, new Rating(1, "test", "test"), null);
+		filmStorage.create(firstFilm);
 		Film secondFilm =  new Film("TestNew", "TestNew", LocalDate.of(2000, 1, 1), 1, 1, new Rating(1, "test", "test"), null);
 		filmStorage.create(secondFilm);
 
@@ -107,7 +126,11 @@ class FilmoRateApplicationTests {
 
 	@Test
 	public void testDeleteFilm() throws NotFoundException, ValidationException {
-		filmStorage.delete(1);
-		assertThrows(NotFoundException.class, () -> filmStorage.read(1));
+		Film testFilm = new Film("Test", "Test", LocalDate.of(2000, 1, 1), 1, 1, new Rating(1, "test", "test"), null);
+		Film createdFilm = filmStorage.create(testFilm);
+		Assertions.assertTrue(filmStorage.contains(createdFilm.getId()));
+
+		filmStorage.delete(createdFilm.getId());
+		Assertions.assertFalse(filmStorage.contains(createdFilm.getId()));
 	}
 }
